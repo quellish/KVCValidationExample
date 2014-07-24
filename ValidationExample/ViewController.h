400cad67230aaa030f187d6206c65953c06df308
quellish <github@quellish.neomailbox.com>
2014-07-24 10:43:12 -0700

    Initial Commit

---
+++ ValidationExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ValidationExample
+//
+//  Created by Dan Zinngrabe on 7/24/14.
+//  Copyright (c) 2014 Dan Zinngrabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ValidationExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ValidationExample
+//
+//  Created by Dan Zinngrabe on 7/24/14.
+//  Copyright (c) 2014 Dan Zinngrabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
